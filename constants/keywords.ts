115470f5705ff238beda37a9afc640c293dc953f
Givikuna <thommaster10@gmail.com>
2026-02-24 16:39:14 -0500

    building base to begin making Lexer/Tokenizer

---
+++ constants/keywords.ts
@@ -0,0 +1,24 @@
+import { Keyword } from "../types/types";
+
+export const keywords: ReadonlyArray<Keyword> = [
+    "READ",
+    "DISPLAY",
+    "SET",
+    "COMPUTE",
+    "ADD",
+    "SUBTRACT",
+    "IF",
+    "THEN",
+    "ENDIF",
+    "TO",
+    "ELSE",
+    "WHILE",
+    "ENDWHILE",
+    "DO",
+    "REPEAT",
+    "UNTIL",
+    "FOR",
+    "ENDFOR",
+    "HALT",
+    "FROM",
+];
---
+++ types/types.ts
@@ -0,0 +1,58 @@
+export type Keyword =
+    | "READ"
+    | "DISPLAY"
+    | "SET"
+    | "COMPUTE"
+    | "ADD"
+    | "SUBTRACT"
+    | "IF"
+    | "THEN"
+    | "ENDIF"
+    | "ELSE"
+    | "WHILE"
+    | "ENDWHILE"
+    | "DO"
+    | "REPEAT"
+    | "UNTIL"
+    | "FOR"
+    | "ENDFOR"
+    | "HALT"
+    | "TO"
+    | "TRUE"
+    | "FALSE"
+    | "FROM";
+
+export enum TokenType {
+    Keyword = "Keyword",
+    Identifier = "Identifier",
+    Number = "Number",
+    String = "String", // ""
+
+    // Operators
+    Plus = "Plus", // +
+    Minus = "Minus", // -
+    Multiply = "Multiply", // *
+    Divide = "Divide", // /
+
+    // Relational Operators
+    Equals = "Equals", // ==
+    NotEquals = "NotEquals", // !=
+    Greater = "Greater", // >
+    Less = "Less", // <
+    GreaterEquals = "GreaterEquals", // >=
+    LessEquals = "LessEquals", // <=
+
+    // Punctuation
+    ParenL = "ParenL", // (
+    ParenR = "ParenR", // )
+
+    EOF = "EOF",
+    Unknown = "Unknown",
+}
+
+export interface Token {
+    type: TokenType;
+    value: string;
+    line: number;
+    column: number;
+}
